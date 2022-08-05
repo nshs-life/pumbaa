df0c8920c9213cee6014bac3c298427fa71b8fc0
Michael C <michaelchangbest@gmail.com>
2022-08-05 09:00:24 -0400

    tutor command

--- commands/tutor.js
+++ commands/tutor.js
@@ -4,23 +4,37 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('tutor')
         .setDescription('request a tutor for your needs')
-        .addChannelOption(option => option.setName('subject').setDescription('Select a channel'))
-        .addStringOption(option => option.setName('request').setDescription('tell tutors what you need help with')),
+
+        //subjects for user to choose from
+        .addStringOption(option =>
+            option.setName('subject')
+                .setDescription('Select a subject')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'Math', value: 'Math' },
+                    { name: 'English', value: 'English' },
+                    { name: 'Science', value: 'Science' },
+                    { name: 'History', value: 'History' },
+                    { name: 'Language', value: 'Language' },
+                    { name: 'Other', value: 'Other' },
+                ))
+
+        //description of what they need help with
+        .addStringOption(option =>
+            option.setName('request')
+                .setDescription('tell tutors what you need help with')
+                .setRequired(true)),
     async execute(interaction) {
 
-        //format embed
-        const channel = interaction.options.getChannel('subject')
-        if (!channel) return interaction.reply({ content: 'please select a subject', ephemeral: true });
-        interaction.guild.channels.fetch(channel)
-            .then(subject => console.log(subject.name))
-        
+        const subject = interaction.options.getString('subject')
         const request = interaction.options.getString('request')
-        if (!request) return interaction.reply({ content: 'please enter a description of what you need help with', ephemeral: true });
+
+        //format embed
         const Embed = new EmbedBuilder()
             .setTitle('New Request:')
             .setDescription('From: ' + interaction.user.tag.split(/#/)[0])
             .setColor(0x18e1ee)
-            .addFields({ name: 'Subject: ', value: 'Description: ' + request });
+            .addFields({ name: 'Subject: ' + subject, value: 'Description: ' + request });
 
         //post request to tutors
         interaction.guild.channels.fetch('1005048112890511450')
